0f3c6b25aa1b273587853145f7058900ecf2061b
ale18-ship <acerap@unicartagena.edu.co>
2025-09-15 14:35:30 -0500

    feat: crear estructura de menú interactivo en Main

--- src/main/java/org/cajero/Main.java
+++ src/main/java/org/cajero/Main.java
@@ -1,17 +1,37 @@
 package org.cajero;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Cajero cajero = new Cajero();
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        cajero.agregarUsuario(new Usuario("Juan", 1, 1000));
+        cajero.agregarUsuario(new Usuario("Maria", 2, 500));
+
+        int opcion;
+        do {
+            System.out.println("\n=== Menú Cajero ===");
+            System.out.println("1. Consultar saldo");
+            System.out.println("2. Depositar");
+            System.out.println("3. Retirar");
+            System.out.println("4. Salir");
+            System.out.print("Elige una opción: ");
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1, 2, 3:
+                    // lógica pendiente
+                    break;
+                case 4:
+                    System.out.println("¡Gracias por usar el cajero!");
+                    break;
+                default:
+                    System.out.println("Opción inválida.");
+            }
+        } while (opcion != 4);
+
+        sc.close();
     }
 }
